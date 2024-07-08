2e55c52efe99abe0abb638c65a94d47200281a5a
Martyn Davies <martynrdavies@gmail.com>
2024-07-08 16:33:50 +0200

    reformat for new langchain approach, add colors to ouput and add additional comments and function descriptions

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
-const { ChatOpenAI } = require("@langchain/openai");
-const { ToolMessage } = require("@langchain/core/messages");
-const axios = require('axios')
+import { ChatOpenAI } from "@langchain/openai";
+import { ToolMessage } from "@langchain/core/messages";
+import colors from "colors";
+import axios from "axios";
 
-const OPENAI_API_KEY = "<your-openai-api-key>";
-const SUPERFACE_AUTH_TOKEN = "<your-superface-auth-token>";
+// Set up constants for the Superface Hub API and the prompt
+
 const SUPERFACE_BASE_URL = "https://pod.superface.ai/api/hub";
 const PROMPT = "What's the weather like in Prague and in Kosice?";
 
 (async () => {
-
+  // @description: Get the tools from the Superface Hub API
   async function getSuperfaceTools() {
     try {
       const response = await axios.get(`${SUPERFACE_BASE_URL}/fd`, {
         headers: {
-          Authorization: `Bearer ${SUPERFACE_AUTH_TOKEN}`,
+          Authorization: `Bearer ${process.env.SUPERFACE_AUTH_TOKEN}`,
         },
       });
       return response.data;
@@ -21,59 +22,71 @@ const PROMPT = "What's the weather like in Prague and in Kosice?";
       console.error(error);
     }
   }
-  
+
+  // @description: Perform the action using the Superface Hub API
   async function performAction(functionName, toolCallArguments) {
+    console.log(
+      `Calling Superface Hub API function ${functionName} with arguments ${JSON.stringify(
+        toolCallArguments
+      )}`["green"]
+    );
     try {
-      const actionResponse = await axios.post(`${SUPERFACE_BASE_URL}/perform/${functionName}`, toolCallArguments,
-        { headers: {
-            Authorization: `Bearer ${SUPERFACE_AUTH_TOKEN}`,
-            'Content-Type': 'application/json',
-            'x-superface-user-id': 'sflangchainexample|123'
-          }
-      });
+      const actionResponse = await axios.post(
+        `${SUPERFACE_BASE_URL}/perform/${functionName}`,
+        toolCallArguments,
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.SUPERFACE_AUTH_TOKEN}`,
+            "Content-Type": "application/json",
+            "x-superface-user-id": "sflangchainexample|martyndavies|08062024",
+          },
+        }
+      );
 
       let result = JSON.stringify(actionResponse.data);
-      console.log(`SUPERFACE RESPONSE: ${result}`);
+      //console.log(`SUPERFACE RESPONSE: ${result}`);
       return result;
-      
     } catch (error) {
       console.error(`PERFORM ERROR: ${error.response}`);
       return error.response.data;
     }
   }
 
-  // Bind function to the model as a tool
-  const chat = new ChatOpenAI({
-    modelName: "gpt-4-1106-preview",
+  // Set up the LLM as GPT-4o using LangChain's ChatOpenAI class
+  const llm = new ChatOpenAI({
+    model: "gpt-4o",
     maxTokens: 128,
-    openAIApiKey: OPENAI_API_KEY,
-  }).bind({
-    tools: await getSuperfaceTools(),
-    tool_choice: "auto",
+    apiKey: process.env.OPENAI_API_KEY,
   });
 
-  // Ask initial question that requires multiple tool calls
-  const res = await chat.invoke([
-    ["human", PROMPT],
-  ]);
+  // Retrieve the tools list from the Superface Hub API
+  const superfaceTools = await getSuperfaceTools();
+
+  // Bind the list of available tools to the LLM
+  const llmWithTools = llm.bindTools(superfaceTools);
+
+  // Invoke the LLM with the initial prompt that requires multiple tool calls
+  const res = await llmWithTools.invoke(PROMPT);
 
   // Format the results from calling the tool calls back to OpenAI as ToolMessages
-  const toolMessages = res.additional_kwargs.tool_calls?.map(async (toolCall) => {
-    const toolCallResult = await performAction(toolCall.function.name, JSON.parse(toolCall.function.arguments));
+  const toolMessages = res.tool_calls?.map(async (toolCall) => {
+    const toolCallResult = await performAction(toolCall.name, toolCall.args);
 
     return new ToolMessage({
       tool_call_id: toolCall.id,
-      name: toolCall.function.name,
-      content: toolCallResult
+      name: toolCall.name,
+      content: toolCallResult,
     });
   });
 
-  // Send the results back as the next step in the conversation
-  const finalResponse = await chat.invoke([
-    ["human", PROMPT],
+  // Re-invoke the LLM with the initial prompt and the tool messages
+  // to continue the conversation and get the final response.
+  const finalResponse = await llmWithTools.invoke([
+    PROMPT,
     res,
-    ...(await Promise.all(toolMessages ?? []))
+    ...(await Promise.all(toolMessages ?? [])),
   ]);
 
-  console.log(finalResponse.content);
+  // Return the initial prompt and the final response
+  console.log("\n\n" + PROMPT["blue"] + "\n\n" + finalResponse.content);
 })();
